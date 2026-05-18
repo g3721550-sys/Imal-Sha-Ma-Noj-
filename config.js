109fe46ab0a20aeab30b403469be86fff0bbd2f8
g3721550-sys <g3721550@gmail.com>
2026-05-17 23:44:08 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/g3721550-sys/Imal-Sha-Ma-Noj-/blob/main/images/FB_IMG_1763822089895.jpg?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Imal Sha MA Noj Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
