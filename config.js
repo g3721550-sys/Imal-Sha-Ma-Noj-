6a20f70240c25306f33f7127c42948baa76f27f5
g3721550-sys <g3721550@gmail.com>
2026-05-21 17:38:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/g3721550-sys/Imal-Sha-Ma-Noj-/blob/main/images/FB_IMG_1763822089895.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Imal Sha MA Noj Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94719394808',  // Replace with the owner's phone number
 
 
 
